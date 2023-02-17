789c6c5cb06c514fd503b3ab1f249208eb63914a
Joun <urbanscratcher@gmail.com>
2023-02-17 18:03:47 +0900

    all

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,37 +1,34 @@
 'use strict';
 
-function calcAge(birthYear){
-    const age = 2037 - birthYear;
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
 
-    function printAge(){
-        let output = `${firstName}, You are ${age}, born in ${birthYear}`;
-        console.log(output);
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
 
-        if(birthYear >= 1981 && birthYear <= 1996){
-            var millenial = true;
-            const firstName = 'Steven';
-            const str = `Oh, and you're a millenial, ${firstName}`;
-            console.log(str);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
 
-            function add(a, b){
-                return a+b;
-            }
+      function add(a, b) {
+        return a + b;
+      }
 
-
-            output = 'NEW OUTPUT!';
-        }
-
-        // console.log(str);
-        console.log(millenial);
-        // console.log(add(2, 3));
-        console.log(output);
-
-
+      output = 'NEW OUTPUT!';
     }
 
-    printAge();
-    
-    return age;
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
 }
 
 const firstName = 'Jonas';
