6260a4c032b2d0014a3dd5279b5db4ea1b87b250
Joun <urbanscratcher@gmail.com>
2023-03-08 22:04:21 +0900

    starter wrote

---
+++ 15-Mapty/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Septempber', 'October', 'November', 'December']
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+//////////////////////////////////////////////////
+//
