b7963e80f2756243b71b07119d5bbb121f13d068
Joun <urbanscratcher@gmail.com>
2023-03-20 19:49:57 +0900

    pagination

---
+++ 18-Forkify/src/js/views/paginationView.js
@@ -0,0 +1,79 @@
+import View from './view.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class PaginationView extends View {
+  _parentEl = document.querySelector('.pagination');
+
+  addHandleClick(handler) {
+    this._parentEl.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    const currPage = this._data.page;
+
+    // Page 1, and there are other pages
+    if (currPage === 1 && numPages > 1) {
+      return `
+        <button data-goto="${
+          currPage + 1
+        }" class="btn--inline pagination__btn--next">
+            <span>Page ${currPage + 1}</span>
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+        </button> 
+      `;
+    }
+
+    // Last Page
+    if (currPage === numPages && numPages > 1) {
+      return `
+        <button data-goto="${
+          currPage - 1
+        }" class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${currPage - 1}</span>
+        </button>
+      `;
+    }
+
+    // Other pages
+    if (currPage < numPages) {
+      return `
+        <button data-goto="${
+          currPage - 1
+        }" class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${currPage - 1}</span>
+        </button>
+        <button data-goto="${
+          currPage + 1
+        }" class="btn--inline pagination__btn--next">
+            <span>Page ${currPage + 1}</span>
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+        </button> 
+      `;
+    }
+
+    // Page 1, and there are No other pages
+    return ``;
+  }
+}
+
+export default new PaginationView();
